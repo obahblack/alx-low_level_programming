e1f870693e2bc30d1bd3462bdcfed312f879e4d6
praise obah <obahsomto@gmail.com>
2022-12-08 07:11:01 -0800

    put problems

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * code
+ * return always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
