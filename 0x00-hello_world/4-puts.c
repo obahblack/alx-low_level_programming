e15697b55afe522f18d0999eca40a29fff9937c9
praise obah <obahsomto@gmail.com>
2022-12-08 07:07:53 -0800

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/*
+/**
  * code
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle"\n);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
