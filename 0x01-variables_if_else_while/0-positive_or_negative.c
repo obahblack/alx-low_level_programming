0dfc96f49d00371a2bbcfee218f6ea597528fe6d
praise obah <obahsomto@gmail.com>
2022-12-09 17:58:53 -0800

    if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 
 /**
- * main = Entry point to generate a random number
+ * main - Entry point to generate a random number
+ *
  * Description - prints if random generated number is +tive, zero or -tive
  * Return: Always 0 (Success)
 */
