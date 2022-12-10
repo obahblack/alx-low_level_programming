38a12c95a3e677563e56fdef2598fa840ac4ec35
praise obah <obahsomto@gmail.com>
2022-12-10 13:54:42 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entrt point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c = 0;
+
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
