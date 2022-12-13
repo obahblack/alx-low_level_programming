80943298019c2dafce35ffbc3cb632a6a01cd624
praise obah <obahsomto@gmail.com>
2022-12-13 14:19:50 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabets - Entry point
+ *
+ * Return: Always 0 (succes)
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while(c <= 'z'){
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+	return (0);
+}
