cfbbb3b801bc88138c23c4fd590358f8909753f7
praise obah <obahsomto@gmail.com>
2022-12-13 15:07:45 -0800

    int _islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#iclude "main.h"
+
+/**
+ * int _islower - Entry point 
+ *
+ * Return: 1 if letter is lowercase, return 0 for othherwise"
+ */
+int _islower(int c)
+{
+	if (c = 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
