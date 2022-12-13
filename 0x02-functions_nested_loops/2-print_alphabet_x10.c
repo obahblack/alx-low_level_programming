7dc22b6cdb3fc873957f5d340aaa56b812dfeecf
praise obah <obahsomto@gmail.com>
2022-12-13 14:52:29 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (succes)
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char c;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+			_putchar('\n')
+		}
+	}
+}
