8d55d184960774f3ca5208e5b9057ea745b7077b
praise obah <obahsomto@gmail.com>
2022-12-15 14:02:41 -0800

    print more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,7 +8,7 @@ void more_numbers(void)
 {
 	int number1, number2;
 
-	for (number1 = 0; number1 < 10; number1++)
+	for (number1 = 0; number1 <= 9; number1++)
 	{
 		for (number2 = 0; number2 <= 14; number2++)
 		{
@@ -18,6 +18,6 @@ void more_numbers(void)
 			}
 			_putchar((number2 % 10) + '0');
 		}
-		_putchar(10);
+		_putchar('\n');
 	}
 }
