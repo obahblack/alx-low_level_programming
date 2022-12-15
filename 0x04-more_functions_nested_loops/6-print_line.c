18bec84cb8176f6b65cef36cadcfcc7fcd78c63f
praise obah <obahsomto@gmail.com>
2022-12-15 14:44:05 -0800

    msars

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - functiion that draws a straight
+ * @n: print int
+ * Return: Always 0
+ */
+void print_line(int n)
+{
+	int 1;
+
+	for (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			putchar(95);
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
