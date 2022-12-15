2387b1fdafc7a2d383cb87d38668395bccd41fc6
praise obah <obahsomto@gmail.com>
2022-12-15 10:03:15 -0800

    p Print

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- * print_numbers - function to print numbers 0 to 9
- *
- * Return: 0-9 followed by a new line
+ * print_numbers - function to print numbers
+ * Return: Always 0
  */
 void print_numbers(void)
 {
