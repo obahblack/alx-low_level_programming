f73fd55694d7253da82b2249841b48246ff06d04
praise obah <obahsomto@gmail.com>
2022-12-19 15:06:34 -0800

    put2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - function that prints every other character of a string
+ * @str: str parameter
+ *
+ * Return: Always 0
+ */
+void puts2(char *str)
+{
+	int len, i;
+
+	len = strlen(str);
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
