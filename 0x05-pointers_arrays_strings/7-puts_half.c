6d2e718c02a5229b3e4d0e9be172de35123a6a96
praise obah <obahsomto@gmail.com>
2022-12-19 15:21:29 -0800

    prints half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - prints half string
+ * @str: string parameter
+ *
+ * Return: always 0
+ */
+void puts_half(char *str)
+{
+	int x, y;
+
+	x = strlen(str);
+	if (x % 2 == 1)
+		y = x / 2 + 1;
+	else
+		y = x / 2;
+	for (i = y; i < x; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
