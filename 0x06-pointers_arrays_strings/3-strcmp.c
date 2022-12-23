89ebc1b2a26bf85d5645f3f1a664c4e5f4724c11
praise obah <obahsomto@gmail.com>
2022-12-23 09:33:35 -0800

    comparing to num

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - a function that compares 2 strings
+ * @s1: param1
+ * s2: param2
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
