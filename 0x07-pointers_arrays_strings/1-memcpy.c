187a316fcc4eaa10d2377d9096928763a9c487cf
praise obah <obahsomto@gmail.com>
2023-01-02 05:34:32 -0800

    memory copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "maiin.h"
+/**
+ * _memcpy - function that copies n bytes from memory src to dest
+ * @dest: destination
+ * @src: memory area
+ * @n: interger
+ * Return: destination Dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		dest[a] = src[a];
+	return (dest);
+}
