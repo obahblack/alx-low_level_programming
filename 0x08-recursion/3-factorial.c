fde3fb00b93bf47f125386cdc3c05d02b9626e55
praise obah <obahsomto@gmail.com>
2023-01-04 07:50:31 -0800

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * factorial - returns factorial
+ * @n: number to be checked
+ * Return: factorial of a number
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	return (n * factorial(n - 1));
+}
