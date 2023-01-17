cfd05a4f9c6d0cd3e56181d3cb2cbe951c0881fb
praise obah <obahsomto@gmail.com>
2023-01-16 16:42:21 -0800

    struct tf

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-#include "main.h"
+#include <stdlib.h>
 /**
  * init_dog - initialize a variable of struct
  * @d: age
@@ -11,9 +11,9 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		return (NULL);
+		return;
 
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 }
