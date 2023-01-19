8f90c4e662412ce8be7fa66c2616106733d4ac67
praise obah <obahsomto@gmail.com>
2023-01-19 11:36:57 -0800

    A function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,90 +1,51 @@
 #include "variadic_functions.h"
-
+#include <stdio.h>
+#include <stdarg.h>
 /**
- * p_char - print characters
- * @ap: input char
- * Return: void
- */
-void p_char(va_list ap)
-{
-	printf("%c", va_arg(ap, int));
-}
-
-/**
- * p_num - print number
- * @ap: input number
- * Return: void
- */
-void p_num(va_list ap)
-{
-	printf("%d", va_arg(ap, int));
-}
-
-/**
- * p_float - print float
- * @ap: input float
- * Return: void
- */
-void p_float(va_list ap)
-{
-	printf("%f", va_arg(ap, double));
-}
-
-/**
- * p_string - print string
- * @ap: input string
- * Return: void
- */
-void p_string(va_list ap)
-{
-	char *string;
-
-	string = va_arg(ap, char *);
-	if (string == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", string);
-
-}
-
-/**
- * print_all - print out integer, float, char, or string
- * @format: format string of input
- * Return: void
+ * print_all - prints parameteres of any type
+ * @format: list of type of var
+ * Return: anything
  */
 void print_all(const char * const format, ...)
 {
-	int i, k;
-	char *separator;
-	form_t data_types[] = {
-		{"c", p_char},
-		{"i", p_num},
-		{"f", p_float},
-		{"s", p_string},
-		{NULL, NULL}
-	};
-	va_list ap;
+	va_list param;
+	int i = 0, success;
+	char *strcopy;
 
-	va_start(ap, format);
-	i = 0;
-	separator = "";
-	while (format && format[i] != '\0')
+	va_start(param, format);
+	while (format && format[i])
 	{
-		k = 0;
-		while (data_types[k].type != NULL)
+		success = 1;
+		switch (format[i])
 		{
-			if ((data_types + k)->type[0] == format[i])
+		case 'c':
+			printf("%c", va_arg(param, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(param, int));
+			break;
+		case 's':
+			strcopy = va_arg(param, char*);
+			if (strcopy == NULL)
 			{
-				printf("%s", separator);
-				data_types[k].func(ap);
-				separator = ", ";
+				printf("(nil)");
+				break;
 			}
-			k++;
+			printf("%s", strcopy);
+			break;
+		case 'f':
+			printf("%f", va_arg(param, double));
+			break;
+		default:
+			success = 0;
+			break;
+		}
+		if (format[i + 1] && success == 1)
+		{
+			printf(", ");
 		}
 		i++;
 	}
-	va_end(ap);
 	printf("\n");
+	va_end(param);
 }
