2fb4ca385a6f12b604f9a3b92f121ff2fecbf72d
praise obah <obahsomto@gmail.com>
2023-01-19 11:06:37 -0800

    function

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,20 +8,19 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list param;
-
-	va_start(param, n);
-	int sum = 0;
-	unsigned int n = 0;
+	int k = 0;
+	va_list ap;
+	unsigned int i;
 
 	if (n == 0)
 	{
 		return (0);
 	}
+	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(param, unsigned int);
+		k += va_arg(ap, int);
 	}
-	va_end(param);
-	return (sum);
+	va_end(ap);
+	return (k);
 }
