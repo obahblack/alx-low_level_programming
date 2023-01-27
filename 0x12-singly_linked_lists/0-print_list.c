76059982414b9e3e3868f3ce829caaed5a8f8010
praise obah <obahsomto@gmail.com>
2023-01-27 09:12:53 -0800

    0x12. C - Singly linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_list - prints elements of a given list
+ * @h: pointer to the list
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t numberElements;
+
+	numberElements = 0;
+	while (h != NULL)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		numberElements++;
+	}
+	return (numberElements);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,6 @@
+Project : 0x11. C - Singly linked lists Tasks: 0. prints all the elements of a given list.
+
+returns the number of elements in a linked list.
+adds a new node at the beginning of the list.
+adds a new node at the end of the list.
+frees the list.
