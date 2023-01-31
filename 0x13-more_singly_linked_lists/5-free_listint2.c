095b0923bba8bf8d3c680f0cc5364ecb3ffa2c14
praise obah <obahsomto@gmail.com>
2023-01-31 09:02:20 -0800

    free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free nodes and set head to NULL
+ * @head: head node
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+
+	while (head != NULL && *head != NULL)
+	{
+		temp = *head;
+		*head = temp->next;
+		free(temp);
+	}
+	head = NULL;
+
+}
