d4649914cc33ecc155098b05c0cab421b42472aa
praise obah <obahsomto@gmail.com>
2023-02-11 08:45:27 -0800

    ok

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,20 +6,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 /**
- * _strlen - finds string length
- * @s: a string
- * Return: string length
- */
-int _strlen(char *s)
-{
-	int length;
-
-	length = 0;
-	while (s[length] != '\0')
-		length++;
-	return (length);
-}
-/**
  * create_file - creates a file
  * @filename: pointer to the file to be created
  * @text_content: terminated string to write to the file
@@ -27,34 +13,34 @@ int _strlen(char *s)
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int write_to_file;
-	int length;
 
+	int fd;
+	ssize_t bytes_written;
+	
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-
+	
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 	{
 		return (-1);
 	}
-
+	
 	if (text_content == NULL)
 	{
 		close(fd);
 		return (1);
 	}
-
-	length = _strlen(text_content);
-	write_to_file = write(fd, text_content, length);
-	if (write_to_file == -1)
+	
+	bytes_written = write(fd, text_content, strlen(text_content));
+	if (bytes_written == -1 || bytes_written != strlen(text_content))
 	{
 		close(fd);
 		return (-1);
 	}
+	
 	close(fd);
 	return (1);
 }
