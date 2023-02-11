58bddf14ef4c53b03ff1103c9aea5918dcd01f42
praise obah <obahsomto@gmail.com>
2023-02-11 08:07:48 -0800

    commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
  * create_file - a function that creates a file
  * @filename: is the name of the file to be created
  * @text_content: is the string to be written to the file
- * Return: ! on sucess, -1 on failure
+ * Return: 1 on sucess, -1 on failure
  */
 int create_file(const char *filename, char *text_content);
 {
