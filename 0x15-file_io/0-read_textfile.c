1c7c7986b8b255d1e28c4a61e11d786b2d6831ab
praise obah <obahsomto@gmail.com>
2023-02-11 06:21:33 -0800

    hello

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -28,4 +28,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 	{
 		return (0);
-
+	}
+	if (filename == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	letters_read = read(fd, buf, letters);
+	letters_written = write(STDOUT_FILENO, buf, letters_read);
+	if (letters_read == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	if (letters_written == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	close(fd);
+	return (letters_written);
+}
